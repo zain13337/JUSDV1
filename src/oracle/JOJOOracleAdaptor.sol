51bc3964ac119cdfde29e37251f648b9d395bc01
JoscelynFarr <fushikun307@outlook.com>
2023-05-24 14:42:48 +0800

    Sherlock-audit-173-Missing check of oracle's return price

--- src/oracle/JOJOOracleAdaptor.sol
+++ src/oracle/JOJOOracleAdaptor.sol
@@ -9,6 +9,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import { IChainLinkAggregator } from "../Interface/IChainLinkAggregator.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import "../lib/JOJOConstant.sol";
+import "../utils/JUSDError.sol";
 
 contract JOJOOracleAdaptor is IPriceChainLink, Ownable {
     address public immutable chainlink;
@@ -25,12 +26,16 @@ contract JOJOOracleAdaptor is IPriceChainLink, Ownable {
 
     function getAssetPrice() external view override returns (uint256) {
         /*uint80 roundID*/
-        (, int256 price,, uint256 updatedAt,) = IChainLinkAggregator(chainlink).latestRoundData();
-        (, int256 USDCPrice,, uint256 USDCUpdatedAt,) = IChainLinkAggregator(USDCSource).latestRoundData();
+        (uint80 roundID, int256 rawPrice, , uint256 updatedAt, uint80 answeredInRound) = IChainLinkAggregator(chainlink).latestRoundData();
+        (uint80 USDCRoundID, int256 USDCPrice, , uint256 USDCUpdatedAt, uint80 USDCAnsweredInRound) = IChainLinkAggregator(USDCSource).latestRoundData();
+        require(rawPrice> 0, JUSDErrors.CHAINLINK_PRICE_LESS_THAN_0);
+        require(answeredInRound >= roundID, JUSDErrors.STALE_PRICE);
+        require(USDCPrice> 0, JUSDErrors.USDC_CHAINLINK_PRICE_LESS_THAN_0);
+        require(USDCAnsweredInRound >= USDCRoundID, JUSDErrors.USDC_STALE_PRICE);
 
         require(block.timestamp - updatedAt <= heartbeatInterval, "ORACLE_HEARTBEAT_FAILED");
         require(block.timestamp - USDCUpdatedAt <= heartbeatInterval, "USDC_ORACLE_HEARTBEAT_FAILED");
-        uint256 tokenPrice = (SafeCast.toUint256(price) * 1e8) / SafeCast.toUint256(USDCPrice);
+        uint256 tokenPrice = (SafeCast.toUint256(rawPrice) * 1e8) / SafeCast.toUint256(USDCPrice);
         return tokenPrice * JOJOConstant.ONE / decimalsCorrection;
     }
 }
